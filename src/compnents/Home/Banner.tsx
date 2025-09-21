d090c5240cef974887d215519be70671f50fcac8
ganesh tharu <tharuganesh987@gmail.com>
2025-09-21 17:21:36 +0545

    refactor:bannner image

--- src/compnents/Home/Banner.tsx
+++ src/compnents/Home/Banner.tsx
@@ -1,10 +1,9 @@
-
 import React from "react";
 import { BsShieldLockFill } from "react-icons/bs";
 import TestimonialSlider from "./TestimonialSlider";
 import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa";
-import Link  from "next/link";
+import Link from "next/link";
 
 function Banner() {
   return (
@@ -12,33 +11,33 @@ function Banner() {
       <div className="flex flex-col md:flex-row w-full h-auto md:h-[60vh]">
         
         {/* Left Card */}
-        <div className="flex-1 bg-gray-200 flex flex-col justify-center items-start p-6 sm:p-8">
+        <div className="w-full md:flex-1 bg-gray-200 flex flex-col justify-center items-start p-6 sm:p-8">
           <div className="text-2xl sm:text-3xl text-black mb-3 sm:mb-4">
             <BsShieldLockFill />
           </div>
           <h2 className="text-lg sm:text-xl md:text-2xl font-bold mb-3 sm:mb-4 text-gray-950">
             Litigation & Dispute Resolution
           </h2>
-           
-          <button className="flex items-center gap-2 text-m sm:text-base font-medium text-black hover:text-blue-600">
-          <Link href="/services">Services <FaArrowRight /></Link>
-          </button>
-         
+          <Link
+            href="/services"
+            className="flex items-center gap-2 text-sm sm:text-base font-medium text-black hover:text-blue-600"
+          >
+            Services <FaArrowRight />
+          </Link>
         </div>
 
         {/* Middle Image */}
-        <div className="flex-1 relative h-64 sm:h-80 md:h-full">
-  <Image
-    src="/ladystatue.jpg"
-    alt="Lady Statue"
-    fill
-    className="object-cover"
-  />
-</div>
-
+        <div className="w-full md:flex-1 relative h-64 sm:h-80 md:h-full">
+          <Image
+            src="/ladystatue.jpg"
+            alt="Lady Statue"
+            fill
+            className="object-cover"
+          />
+        </div>
 
         {/* Right Testimonial */}
-        <div className="flex-1 bg-blue-100 flex flex-col justify-center p-6 sm:p-8 text-white">
+        <div className="w-full md:flex-1 bg-blue-100 flex flex-col justify-center p-6 sm:p-8 text-gray-900">
           <TestimonialSlider />
         </div>
       </div>
